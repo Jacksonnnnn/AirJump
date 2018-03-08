e1e2452a0b03a9feebe111a3f7164616b301aaa5
Jacksonnn <jhuse01@gmail.com>
2018-03-07 20:34:42 -0500

    Update AirJumpListener.java

--- src/com/Jacksonnn/AirJump/AirJumpListener.java
+++ src/com/Jacksonnn/AirJump/AirJumpListener.java
@@ -3,14 +3,14 @@ package com.Jacksonnn.AirJump;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 import com.projectkorra.projectkorra.BendingPlayer;
 
 public class AirJumpListener implements Listener {
 	
 	@EventHandler
-	public void onShift(PlayerAnimationEvent event) {
+	public void onShift(PlayerToggleSneakEvent event) {
 		Player player = event.getPlayer();
 		BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
 		
